414d3f32f4875a88ff0e49447792eda512e2c0ec
Jhoan B. W. Zevallos Ruvina <johan.zevallos@gmail.com>
2018-05-16 11:23:39 -0500

    2

--- src/main/java/mlt/jenkins/HelloWorld.java
+++ src/main/java/mlt/jenkins/HelloWorld.java
@@ -6,8 +6,11 @@ import org.apache.commons.lang3.StringUtils;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        for (int i = 0; i <25; i++) {
-            System.out.println("Contador :" +i);
+        for (int j = 0; j < 30; j++) {
+            for (int i = 0; i <25; i++) {
+                System.out.println("Contador :" +(i+1)*(j+1));
+            }
+
         }
         final int maxWidth = 8;
         System.out.println(StringUtils.abbreviate("HOLA SOY UNA MCADENA QUE SERA ABREVIADA",maxWidth));
